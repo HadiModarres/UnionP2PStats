eb0fd71bc3a94f394aa6a4b8191affebad36d2f4
hadi <modarres.zadeh@gmail.com>
2019-10-02 15:00:33 +1000

    signalling servers

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var stats = require('../stats');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { stats: JSON.parse(stats.printStats()) });
+  // res.sendFile(__dirname+"/../../orbp2p/index.html");
 });
 
 module.exports = router;
